b057399554ce641ad248729098dc587ff4753d8c
mspangler <mspangler@gmail.com>
2012-12-26 20:47:38 -0600

    refactored and added audio files

--- chico.rb
+++ chico.rb
@@ -12,32 +12,20 @@ class ChicoWatch
   def initialize
     pubnub = Pubnub.new(:subscribe_key => 'sub-c6a6acee-4672-11e0-af21-13b052347a9b')
     pubnub.subscribe(:channel => :dog_channel,
-                     :callback => lambda { |message| handle(message) })
+                     :callback => lambda { |message| parse(message) })
   end
 
   # Parse the message received from the video monitor
   # message example: [[{"command":"couch"}],"13565581135598040"]
-  def handle(message)
+  def parse(message)
     json = message[0][0]
-    process(json['command'], message[1]) unless json.blank?
-  end
-
-  # Process the command
-  def process(command, time_token)
-    puts "Received command #{command} at #{time_token}"
-
-    # Play an mp3 based on the command
-    case command
-    when 'couch'
-      play "test"
-    when 'horse_playing'
-      play "Chico! Stop It!"
-    end
+    play(json['command'], message[1]) unless json.blank?
   end
 
   # Play audio through connected speakers
-  def play(audio)
-    IO.popen("afplay #{audio}.mp3 &")
+  def play(command, time_token)
+    puts "Received command '#{command}' at #{time_token}"
+    IO.popen("afplay audio/#{command}.m4a &")
   end
 
 end
